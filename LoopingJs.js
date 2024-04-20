9dea8ae3e5238859e5c19dd6e01d35942bdd36fb
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-20 21:28:38 +0700

    Create LoopingJs.js

---
+++ LoopingJs.js
@@ -0,0 +1,13 @@
+#!/usr/bin/javascript
+
+// Mengulangi pesan 5 kali dengan for loop
+for (let i = 0; i < 5; i++) {
+  console.log("Nama Saya Adalah Kevin-" + (i+1));
+}
+
+// Mengulangi pesan menggunakan while loop
+let counter = 0;
+while (counter < 5) {
+  console.log("Nama Saya Adalah Kevin-" + (counter+1));
+  counter++;
+}
