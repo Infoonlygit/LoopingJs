265dea2553a5d402e66bd7ca34592836197a2291
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-21 10:50:20 +0700

    Update LoopingJs.js

--- LoopingJs.js
+++ LoopingJs.js
@@ -1,13 +1,13 @@
 #!/usr/bin/javascript
 
-// Mengulangi pesan 5 kali dengan for loop
-for (let i = 0; i < 5; i++) {
-  console.log("Nama Saya Adalah Kevin-" + (i+1));
+// Mengulangi pesan 1500 kali dengan for loop
+for (let i = 0; i < 1500; i++) {
+  console.log("JAVASCRIPT CONFIGURATION-" + (i+1));
 }
 
-// Mengulangi pesan menggunakan while loop
+// Mengulangi pesan 1500 kali menggunakan while loop
 let counter = 0;
-while (counter < 5) {
-  console.log("Nama Saya Adalah Kevin-" + (counter+1));
+while (counter < 1500) {
+  console.log("JAVASCRIPT POPULAR LANGUAGES-" + (counter+1));
   counter++;
 }
